681d54265a671560f9e706166556e750af8e94da
ConradMare890317 <Conradmare@gmail.com>
2021-03-05 21:45:03 +0200

    2021/03/05 (pm)

---
+++ Ordering_operations_using_(Brackets).py
@@ -0,0 +1,7 @@
+BODMAS = 2 * 5 - 1
+print(BODMAS)
+
+#BODMAS = Brackets, Order, Division, Multiplication, Addition, Subtraction
+
+BODMAS = 2 * ( 5 - 1 )
+print(BODMAS)
---
+++ variables.py
@@ -0,0 +1,37 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> number = 1
+>>> number
+1
+>>> NUMBER
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    NUMBER
+NameError: name 'NUMBER' is not defined
+>>> type(number)
+<class 'int'>
+>>> number = "hello"
+>>> type(number)
+<class 'str'>
+>>> 
+>>> print(1+1)
+2
+>>> print(100+100)
+200
+>>> first_number = 1+1
+>>> print(first_number)
+2
+>>> second_number = 100+100
+>>> print(second_number)
+200
+>>> 
+============= RESTART: C:/Users/conra/OneDrive/Desktop/Python(CrashCourse)/Python_bible/Python_bible/variables.py ============
+2
+200
+202
+>>> 
+============= RESTART: C:/Users/conra/OneDrive/Desktop/Python(CrashCourse)/Python_bible/Python_bible/variables.py ============
+7
+200
+207
+>>> 
