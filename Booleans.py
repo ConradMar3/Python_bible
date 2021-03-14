ee3abae07212ce14c96abc38436c8f54a54445af
ConradMare890317 <Conradmare@gmail.com>
2021-03-14 13:41:31 +0200

    2012/03/14 (pm)

---
+++ Booleans.py
@@ -0,0 +1,17 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> B = True
+>>> C = False
+>>> D = True
+>>> E = "True"
+>>> type(E)
+<class 'str'>
+>>> type(B)
+<class 'bool'>
+>>> 2>3
+False
+>>> 2<3
+True
+>>> type(2<3)
+<class 'bool'>
+>>> 
