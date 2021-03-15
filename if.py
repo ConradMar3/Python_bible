64bd126e423685680189bd6312085df8c42833a8
ConradMare890317 <Conradmare@gmail.com>
2021-03-15 12:23:12 +0200

    2021/03/15 (pm)

---
+++ if.py
@@ -0,0 +1,13 @@
+if True:
+    print("It worked")
+
+
+num1 = 100
+num2 = 100
+
+if num1 > num2:
+    print("Num1 is > than Num2")
+elif num2 > num1:
+    print("Num2 is > than Num1")
+else:
+    print("Both are equal")
---
+++ if_statements.py
@@ -0,0 +1,26 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+Num1 is > than Num2
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+Num2 is > than Num1
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+Num2 is > than Num1
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+Both are equal
+>>> 
+========= RESTART: C:/Users/Conrad/Desktop/Python_Udemy/Python_Bible_Repository/Python_bible/if.py ========
+It worked
+Both are equal
+>>> 
